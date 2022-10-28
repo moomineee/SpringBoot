4cae0927f1f7a28dbf7ab7a25b4279919cf1589b
momineee <p.rainmakerm@gmail.com>
2022-10-28 15:28:21 +0900

    @AllArgsConstructor, @Getter 사용으로 코드 간소화

--- src/main/java/com/springboot/hello/domain/User.java
+++ src/main/java/com/springboot/hello/domain/User.java
@@ -1,28 +1,32 @@
 package com.springboot.hello.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
-@Setter
+
+@AllArgsConstructor
+@Getter
 public class User {
     private String id;
     private String name;
     private String password;
-
-    public User(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+//  @AllArgsConstructor와 @Getter Annotation으로 대체 가능.
+//    public User(String id, String name, String password) {
+//        this.id = id;
+//        this.name = name;
+//        this.password = password;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
 }
