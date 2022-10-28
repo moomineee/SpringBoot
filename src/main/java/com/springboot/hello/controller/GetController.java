cca844c1e9615afa4e9c63bfc46635d3cf4241e5
momineee <p.rainmakerm@gmail.com>
2022-10-28 11:34:49 +0900

    @Slf4j Annotation, log.info()를 사용하여 로그 남기기

--- src/main/java/com/springboot/hello/controller/GetController.java
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,20 +1,30 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j // 로그 기록 남기기
 public class GetController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        log.info("getName으로 요청이 들어왔습니다.");
+        return "Kyeongrok";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable로 요청이 들어왔습니다.");
         return variable;
     }
 
