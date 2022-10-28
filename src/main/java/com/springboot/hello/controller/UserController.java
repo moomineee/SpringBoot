24256012be321d86c7a527acecd3ae6d6fce3f76
momineee <p.rainmakerm@gmail.com>
2022-10-28 14:25:06 +0900

    UserController - deletaAll 기능 추가

--- src/main/java/com/springboot/hello/controller/UserController.java
+++ src/main/java/com/springboot/hello/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.UserDao.UserDao;
+경import com.springboot.hello.domain.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -12,5 +19,23 @@ public class UserController {
 
     public UserController(UserDao userDao) {
         this.userDao = userDao;
-    }성
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/user")
+    public User add(User user) throws Exception {
+        userDao.add(new User("1", "kos", "1234"));
+        return userDao.findById("1");
+    }
+
+    @DeleteMapping("/user")
+    public ResponseEntity<Integer> deleteAll() {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(userDao.deleteAll());
+    }
 }
