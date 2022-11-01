1d84c5bb315ec3444b43e8ef3202d2d0d5e29c50
momineee <p.rainmakerm@gmail.com>
2022-11-01 13:28:54 +0900

    HospitalDao - Insert Query에 Value Mapping ?,?,? 뒤에 , 주의! 꼭 붙여야 함. ?,?,?,"가 올바른 형태. 마지막에는 붙이지 않는다.

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -16,11 +16,12 @@ public class HospitalDao {
     // List<Hospital> - 11만건이 들어있음. Hospital
     public void add(Hospital hospital) { // 따라서 Hospital이 들어간다
         String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`) " +
-                     "VALUES ('1', '의원', '3620000', " +
-                     "'PHMA119993620020041100004', '19990612', '1', " +
-                     "'13', '062-515-2875', '광주광역시 북구 풍향동 565번지 4호 3층', " +
-                     "'광주광역시 북구 동문대로 24, 3층 (풍향동)', '효치과의원', '치과의원', " +
-                     "'1', '0', '0', '52.29');";
+                     "VALUES (?, ?, ?," +
+                     "?, ?, ?," + // ? 뒤에 , 잘 확인할 것
+                     "?, ?, ?," +
+                     "?, ?, ?," +
+                     "?, ?, ?," +
+                     "?);"; // 총 16개
         this.jdbcTemplate.update(sql,
                 hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
                 hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),
