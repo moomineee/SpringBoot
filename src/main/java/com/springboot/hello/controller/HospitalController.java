f6bbde868040b5a07c4d1d121874b36d6e9febb9
momineee <p.rainmakerm@gmail.com>
2022-11-02 13:13:44 +0900

    HospitalController로 data 불러오기

--- src/main/java/com/springboot/hello/controller/HospitalController.java
+++ src/main/java/com/springboot/hello/controller/HospitalController.java
@@ -2,7 +2,7 @@ package com.springboot.hello.controller;
 
 import com.springboot.hello.dao.HospitalDao;
 import com.springboot.hello.domain.Hospital;
-import io.swagger.models.auth.In;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,15 +20,15 @@ public class HospitalController {
         this.hospitalDao = hospitalDao;
     }
 
-    @GetMapping("/{id")
+    @GetMapping("/{id}")
     public ResponseEntity<Hospital> get(@PathVariable Integer id) {
         Hospital hospital = hospitalDao.findById(id);
         Optional<Hospital> opt = Optional.of(hospital);
 
-        if () {
-
+        if (!opt.isEmpty()) {
+            return ResponseEntity.ok().body(hospital);
         } else {
-
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Hospital());
         }
     }
 }
